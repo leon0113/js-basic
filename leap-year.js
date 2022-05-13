e5763b30ee1ca0771f628e790e2da86b8203e1a2
leon0113 <tahjib.leon399@gmail.com>
2022-05-14 05:46:46 +0600

    leap-year

--- leap-year.js
+++ leap-year.js
@@ -1,11 +1,33 @@
 function isLeapYear(year) {
-    if (year % 4 == 0) {
+    if (year % 100 == 0 ? year % 400 == 0 : year % 4 == 0) {
         return true;
     }
-    else {
-        return false;
-    }
+    return false;
+
 }
-const myYear = 2027;
+const myYear = 1900;
 const checkMyYear = isLeapYear(myYear);
 console.log(checkMyYear);
+
+
+
+
+
+// function leapYear(years) {
+//     var result;
+//     if (years / 400) {
+//         result = true
+//     }
+//     else if (years / 100) {
+//         result = false
+//     }
+//     else if (years / 4) {
+//         result = true
+//     }
+//     else {
+//         result = false
+//     }
+//     return result
+// }
+// var output = leapYear(2100);
+// console.log(output);
