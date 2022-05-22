54576ee45dcc4e85e45281e4441c67b1b17cd4ba
leon0113 <tahjib.leon399@gmail.com>
2022-05-23 02:05:15 +0600

    recursion example

---
+++ simple.js
@@ -0,0 +1,14 @@
+for (let i = 0; i < 6; ++i) {
+    console.log(i);
+}
+
+/* function consoleNumber(i) {
+    if (i > 5) {
+        return;
+    }
+    console.log(i);
+    consoleNumber(i + 1);
+}
+consoleNumber(1);
+ */
+
